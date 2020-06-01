94db610232725a6360eabacc96c0c4be00e0dcfd
PSF1999 <41580298+PSF1999@users.noreply.github.com>
2020-06-01 19:35:25 +0530

    Add files via upload

---
+++ Invert_Tree.java
@@ -0,0 +1,14 @@
+Invert a binary tree.
+
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        TreeNode temp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = temp;
+        return root;
+    }
+}
+
